c9b8310fcd5efefe298af4e21d5fd83dd85921e8
tbduong <tiffaniduong@gmail.com>
2017-05-03 22:56:07 -0700

    ajax able to grab json data of location, city IF in system

--- scripts/app.js
+++ scripts/app.js
@@ -19,31 +19,38 @@ console.log( "Up and running!" );
       Geo.lat = position.coords.latitude;
       Geo.lng = position.coords.longitude;
       console.log(Geo.lat + "," + Geo.lng );
-  }
 
-//hit API
-  var key = "c2197568420afa6b";
-  var Weather = "http://api.wunderground.com/api/" + key + "forecast/geolookup/conditions/q/" + Geo.lat + "," + Geo.lng + ".json";
-  console.log(Weather);
+      var key = "c2197568420afa6b";
+      // var Weather = "http://api.wunderground.com/api/" + key + "forecast/geolookup/q/" + Geo.lat + "," + Geo.lng + ".json";
+      //Use San Francisco location for Now
+      var Weather = "http://api.wunderground.com/api/c2197568420afa6b/geolookup/q/37.776289,-122.395234.json";
+      console.log(Weather);
 
-  $.ajax({
-      url: Weather,
-      dataType: "jsonp",
-      success: function(data) {
-        //Get all information
-        console.log(data);
-        // var location = data.location;
-        // //setting the spans to the correct parameters
-        // $('#location').html(location);
-      }
-
-
-
-
-
+      $.ajax({
+          url: Weather,
+          dataType: "jsonp",
+          success: function(data) {
+            //Get all information
+            console.log(data);
+            // var location = data.location;
+            // //setting the spans to the correct parameters
+            // $('#location').html(location);
+          }
 
 
   });
+
+
+
+
+
+
+
+
+
+
+
+  }
 
 
 
