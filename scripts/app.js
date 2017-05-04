cab71b783cf00c8611e58b0fde4f84a9c00237e8
tbduong <tiffaniduong@gmail.com>
2017-05-03 22:32:10 -0700

    success-- able to grab current location, still unable to get json from current location endpoint

--- scripts/app.js
+++ scripts/app.js
@@ -18,17 +18,26 @@ console.log( "Up and running!" );
   function success(position){
       Geo.lat = position.coords.latitude;
       Geo.lng = position.coords.longitude;
+      console.log(Geo.lat + "," + Geo.lng );
   }
+
 //hit API
   var key = "c2197568420afa6b";
   var Weather = "http://api.wunderground.com/api/" + key + "forecast/geolookup/conditions/q/" + Geo.lat + "," + Geo.lng + ".json";
+  console.log(Weather);
 
   $.ajax({
       url: Weather,
       dataType: "jsonp",
       success: function(data) {
         //Get all information
+        console.log(data);
+        // var location = data.location;
+        // //setting the spans to the correct parameters
+        // $('#location').html(location);
       }
+
+
 
 
 
