f201c0f702bd1a98f1068e0e38d45be79aa3dfe2
tbduong <tiffaniduong@gmail.com>
2017-05-03 21:33:54 -0700

    error and success function for geolocation

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,28 @@
 $(document).ready(function() {
-    console.log( "Up and running!" );
+  console.log( "Up and running!" );
+
+  //placeholder for longitude, latitude coordinates
+  var Geo={};
+
+  //Create success, error functions to check if browser can support geolocation
+  if (navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(success, error);
+  } else {
+      alert("Geolocation is not supported on this browser.");
+  }
+  //error function
+  function error(){
+      alert("Uh oh! We could not find you.");
+  }
+  //success function
+  function success(position){
+      Geo.lat = position.coords.latitude;
+      Geo.lng = position.coords.longitude;
+  }
+
+
+
+
 
 
 
