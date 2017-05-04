2a9cfd63363b1e7176646df4e7bd7b6c1f94b42d
tbduong <tiffaniduong@gmail.com>
2017-05-03 22:09:57 -0700

    setup ajax call to get data

--- scripts/app.js
+++ scripts/app.js
@@ -1,28 +1,40 @@
 $(document).ready(function() {
-  console.log( "Up and running!" );
+console.log( "Up and running!" );
 
-  //placeholder for longitude, latitude coordinates
+//placeholder for longitude, latitude coordinates
   var Geo={};
 
-  //Create success, error functions to check if browser can support geolocation
+//Create success, error functions to check if browser can support geolocation
   if (navigator.geolocation){
       navigator.geolocation.getCurrentPosition(success, error);
   } else {
       alert("Geolocation is not supported on this browser.");
   }
-  //error function
+//error function
   function error(){
       alert("Uh oh! We could not find you.");
   }
-  //success function
+//success function
   function success(position){
       Geo.lat = position.coords.latitude;
       Geo.lng = position.coords.longitude;
   }
-
+//hit API
   var key = "c2197568420afa6b";
   var Weather = "http://api.wunderground.com/api/" + key + "forecast/geolookup/conditions/q/" + Geo.lat + "," + Geo.lng + ".json";
 
+  $.ajax({
+      url: Weather,
+      dataType: "jsonp",
+      success: function(data) {
+        //Get all information
+      }
+
+
+
+
+
+  });
 
 
 
